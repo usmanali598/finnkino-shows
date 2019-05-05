f5c10b3bbcc28a5f07096dfe817176f0dd1ba125
Usman Ali <usman_pervaiz@live.com>
2019-05-05 13:44:16 +0300

    fetching shows on areaId

--- src/component/FInnkinoShows.js
+++ src/component/FInnkinoShows.js
@@ -15,6 +15,15 @@ class FinnkinoShows extends Component {
   componentDidMount() {
     this.getTexts();
   }
+  changeMovies = ( e ) =>
+  {
+      if ( e.key === 'Enter' )        
+      {
+          console.log( this.refs.typa.value )
+          this.setState( { areaId: this.refs.typa.value } )
+          this.getTexts()
+      }
+  }
   getTexts = () => {
     var selfa = this.state.areaId;
     var today = moment().format("DD.MM.YYYY");
@@ -32,7 +41,6 @@ class FinnkinoShows extends Component {
             showStart: result.Schedule.Shows[ 0 ].Show.map( items => items.TheatreAuditorium[ 0 ] ),
             oneShow: result.Schedule.Shows[ 0 ].Show,
         } )
-
         });
       });
   };
@@ -40,26 +48,29 @@ class FinnkinoShows extends Component {
     var title = this.state.name.map( items => items );
     var imagess = this.state.imags.map( items => items );
     var showTime = this.state.showStart.map( items => items );
-    var style = { width: '1080px' }
-    var styleTitle = { width: '40%' }
-    var styleTime = { width: '20%' }
+    var style = { width: '100%', display:'flex' }
+    var styleTitle = { width: '40%',display:'flex', justifyContent: 'center',alignItems: 'center', background: 'black', color: 'gold' }
+    var styleTime = { width: '20%', display:'flex', justifyContent: 'center',alignItems: 'center', background: 'black', color: 'gold' }
     var styleSali = { color: 'green', border: '1px solid black' }
     return (
-        <div style={ style }>
+        <div >
         <input type='text' ref='typa' placeholder="Cinemas areaId & enter" onKeyPress={ this.changeMovies } />
         <p>Type Cinema Id like 1021, 1022 or 1013 or 1032, 1039 and Press Cinema/Enter then Fetch Button </p>
         <h1 className="showText">Shows Timing </h1>
         <button className="fetchBtn" onClick={ this.getTexts }>Fetch</button>
-        <table className="table">
-            <tbody>
-                { this.state.oneShow.map( ( items, i ) => <tr key={ i } style={ style } className="table">
-                    <td className="table" style={ styleTime }>{ items.dttmShowStartUTC.map( ls => ls.substring( 11, 16 ) ) }</td>
-                    <td > <img src={ items.Images[ 0 ].EventMediumImagePortrait[ 0 ] } height="200px" width="150px" /></td>
-                    <td className="styleTitle" style={ styleTitle }>{ items.Title } </td>
-                    <td className="table" >{ items.TheatreAndAuditorium.toString().split( ',' ) }</td>
-                </tr> ) }
-            </tbody>
-        </table>
+                { this.state.oneShow.map( ( items, i ) => <div key={ i } style={ style } >
+                    <div  style={ styleTime }>{ items.dttmShowStartUTC.map( ls => ls.substring( 11, 16 ) ) }</div>
+                    <div  style={ styleTitle }>{ items.Title } </div>
+                    <div > <img src={ items.Images[ 0 ].EventMediumImagePortrait[ 0 ] } height="200px" width="150px" /></div>
+                    {console.log(items.TheatreAndAuditorium)}
+                    <div  style={ styleTitle } >
+                   <ul style={{listStyle:'none'}}>
+                       <li>{ items.TheatreAndAuditorium.toString().split( ',' )[0] }</li>
+                       <li>{ items.TheatreAndAuditorium.toString().split( ',' )[1] } </li>
+                       <li> { items.TheatreAndAuditorium.toString().split( ',' )[2] }</li>
+                   </ul>
+                   </div>
+                </div> ) }
     </div >
     );
   }
